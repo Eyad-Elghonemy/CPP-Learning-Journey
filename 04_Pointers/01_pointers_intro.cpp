f50598ec14caff8dcc6d6f42d09d7671582ba83a
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-18 00:10:12 +0300

    Add Pointers Folder

---
+++ 04_Pointers/01_pointers_intro.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+/*  Pointers
+
+    What Are Pointers ?
+    --- A Variable That Store Memory Adderess Of Other Variable 
+
+    Why We Need Pointers ?
+    --- To Iterate On Elements In Data Structures Like Array
+    --- Pass Function To Other Function 
+    --- Dynamic Memory Allocation 
+
+    Benefits Of Using Pointers 
+    --- Reduce The Code And Increase Performane 
+
+    Note 
+    --- There's Raw Pointer And Smart Pointer
+
+    Syntax
+    --- Declare Pointer
+    --- Print Variable Memory Address => Refrence Operator || Adress [&]
+    --- Print Value That Memory Address Point To => Defrence Operator[*]
+    --- Change Variable Value With Pointer
+    */
+
+
+
+int main() { 
+
+    // int num = 100;
+    
+    // int *ptr = &num;
+
+    // cout << " Value : " << num << endl; 
+    // cout << " Address : " << &num << endl;
+    
+    // cout << " Address : " << ptr << endl;
+    // cout << " Value : " << *ptr << endl;
+
+    // *ptr = 200;
+
+    // cout << " Value : " << num << endl; 
+    // cout << " Address : " << &num << endl;
+    
+    // cout << " Address : " << ptr << endl;
+    // cout << " Value : " << *ptr << endl;
+
+    cout << "EYAD" << endl;
+
+
+    return 0;
+}
