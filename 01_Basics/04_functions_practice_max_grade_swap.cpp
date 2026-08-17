47e77f16ce32a9cddc44f104a870267d868e47b7
Eyad-Elghonemy <eyad0758@gmail.com>
2026-08-17 06:15:55 +0300

    Add files via upload

---
+++ 01_Basics/04_functions_practice_max_grade_swap.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cmath>
+#include <climits>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+// void add(int n1, int n2);
+// int Max(int n1, int n2);
+// void printGrade(float g);
+// void inc(int &n);
+// void Swap(int &n1, int &n2);
+
+int main(){
+
+    srand(time(0));
+    // int n1, n2, m;
+
+    // cout << "Enter The Two Numbers : " << endl;
+    // cin >> n1 >> n2;
+
+    // m = Max(n1, n2);
+
+    // cout << "Max : " << m << endl;
+
+    // add(n1, n2);
+
+    // float g;
+    // cout << "Enter Your Grade Degree : " << endl;
+    // cin >> g;
+
+    // printGrade(g);
+
+    // int n;
+    // cout << "Enter The Number : " << endl;
+    // cin >> n;
+
+    // cout << "Before Increment : "  << n << endl;
+
+    // inc(n);
+
+    // cout << "After Increment : "  << n;
+
+    // int n1, n2;
+    // cout << "Enter The Two Numbers : " << endl;
+    // cin >> n1 >> n2;
+
+    // cout << "Before Swap\nN1 : " << n1 << "\nN2 : " << n2 << endl;
+    // Swap(n1, n2);
+    // cout << "After Swap\nN1 : " << n1 << "\nN2 : " << n2 << endl;
+
+    
+
+
+
+    return 0;
+}
+
+// void add(int n1, int n2){
+//     cout << n1 + n2;
+// }
+
+// int Max(int n1, int n2){
+//     if(n1>n2)
+//         return n1;
+
+//     else 
+//         return n2;
+// }
+
+// void printGrade(float g){
+//     if(g>=0 && g<=100){
+//         if(g>=90)
+//             cout << "Your Grade : A\n";
+//         else if(g>=80)
+//             cout << "Your Grade : B\n";
+//         else if(g>=70)
+//             cout << "Your Grade : C\n";
+//         else if(g>=60)
+//             cout << "Your Grade : D\n";
+//         else
+//             cout << "Your Grade : F\n";
+//     }
+//     else
+//         cout << "Invalid Input\n";
+// }
+
+// void inc(int &n){
+//     n++;
+//     return ;
+// }
+
+// void Swap(int &n1, int &n2){
+//     int temp = n1;
+//     n1 = n2;
+//     n2 = temp;
+// }
